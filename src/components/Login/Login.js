37d7ba16cdd3cea22b5d12082d1c294b827f2848
RAKIBUL HASAN <rakibul15-13011@diu.edu.bd>
2022-04-12 03:39:07 +0600

    set onblur all input fild and password validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,12 +11,12 @@ const Login = () => {
                         <h2 className='font-title'>Login</h2>
                         <div className="input-group">
                             <label htmlFor='email'>Email</label>
-                            <input type="email" name="email" id="" />
+                            <input type="email" name="email" id="" required/>
                         </div>
 
                         <div className="input-group">
                             <label htmlFor='password'>Password</label>
-                            <input type="password" name="password" id="" />
+                            <input type="password" name="password" id="" required/>
                         </div>
                         <input className='form-submit' type="submit" value="Login" />
                     </div>
--- src/components/SignUp/SignUP.js
+++ src/components/SignUp/SignUP.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css'
 
 const SignUP = () => {
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const[confirmPassword, setConfirmPassword] = useState('')
+    const[error, setError] = useState('')
+
+    const handelEmailBlur = event =>{
+        setEmail(event.target.value)
+    }
+
+    const handelPasswordBlur = event =>{
+        setPassword(event.target.value)
+    }
+    
+    const handelConfirmPasswordBlur = event =>{
+        setConfirmPassword(event.target.value)
+    }
+    
+    const handelCreateUser = event =>{
+        event.preventDefault();
+        if(password !== confirmPassword){
+            setError('Password did not match')
+            return;
+        }
+    }
+
+
     return (
+        <div className='form-container'>
         <div>
-            <h1>This is signUp</h1>
+            <form onSubmit={handelCreateUser}> 
+                <div>
+                    <h2 className='font-title'>SignUp</h2>
+                    <div className="input-group">
+                        <label htmlFor='email'>Email</label>
+                        <input onBlur={handelEmailBlur} type="email" name="email" id="" required/>
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor='password'>Password</label>
+                        <input onBlur={ handelPasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor='confirm-password'>Confirm Password</label>
+                        <input onBlur={handelConfirmPasswordBlur} type="password" name="confirm-password" id="" required/>
+                    </div>
+                    <p style={{color:'red'}}>{error}</p>
+                    <input className='form-submit' type="submit" value="SignUp" />
+                </div>
+            </form>
+            <p>
+                Already Have an account? <Link className='form-link' to="/login">Login</Link>
+            </p>
         </div>
+    </div>
     );
 };
 
